1b02c3f68f4cc4f98d6573380cceb398b6f9afd5
David Quezada <david.quezada.m@gmail.com>
2023-12-17 09:15:16 -0300

    missing param

--- TallerNode/services/pokemon.service.factory.js
+++ TallerNode/services/pokemon.service.factory.js
@@ -2,7 +2,7 @@ const { PokemonService } = require('./pokemonMongo.service');
 const { PokemonMongoService } = require('./pokemonMongo.service');
 
 class PokemonServiceFactory {
-    async getPokemonService() {
+    async getPokemonService(mongoDB) {
         if(mongoDB) return new PokemonMongoService();
         return new PokemonService();
     }
